4ad85738fa8ad1043499d10e45399b1f2bcc4f8a
jho00 <artemze985@gmail.com>
2022-10-15 21:40:20 +0400

    updated android

--- mobile/app/src/main/java/com/example/liquidgold/services/API.kt
+++ mobile/app/src/main/java/com/example/liquidgold/services/API.kt
@@ -3,6 +3,7 @@ package com.example.liquidgold.services
 import android.app.Activity
 import android.util.Log
 import com.example.liquidgold.dto.LoginResponseDTO
+import com.example.liquidgold.models.GeoPoint
 import com.example.liquidgold.models.Task
 import com.example.liquidgold.models.User
 import com.fasterxml.jackson.databind.DeserializationFeature
@@ -13,6 +14,7 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import okhttp3.*
+import java.util.concurrent.TimeUnit
 
 const val DEFAULT_PICTURE_LINK = "https://i.ytimg.com/vi/IMAUjV8vJuM/sddefault.jpg"
 const val BASE_URL = "http://10.0.2.2:8080/"
@@ -26,26 +28,33 @@ var objectMapper: ObjectMapper =
 
 fun getTask(activity: Activity): Observable<Task> {
     return Observable.fromCallable {
-        val request = Request.Builder()
-            .url(BASE_URL.plus("ticket/find/my"))
-            .get()
-            .addHeader("Authorization", "Bearer " + getAuthToken(activity))
-            .build()
-
-        try {
-            client.newCall(request).execute().use { response ->
-                val result = response.body!!.string()
-                Log.e(TAG, result)
-
-                return@fromCallable objectMapper.readValue<Task?>(result, jacksonTypeRef())
-            }
-        } catch (e: Exception) {
-            Log.e(TAG, e.message + " error")
-            throw e
-        }
+        return@fromCallable Task(
+            "Перевезти золото на карьер 2",
+            "В машине под номер е777кх777 необходимо перевезти 4 тонны золота к 14 часам этого дня",
+        3,
+        GeoPoint(51.553842, 45.928540)
+        )
+//        val request = Request.Builder()
+//            .url(BASE_URL.plus("ticket/find/my"))
+//            .get()
+//            .addHeader("Authorization", "Bearer " + getAuthToken(activity))
+//            .build()
+//
+//        try {
+//            client.newCall(request).execute().use { response ->
+//                val result = response.body!!.string()
+//                Log.e(TAG, result)
+//
+//                return@fromCallable objectMapper.readValue<List<Task>?>(result, jacksonTypeRef())[0]
+//            }
+//        } catch (e: Exception) {
+//            Log.e(TAG, e.message + " error")
+//            throw e
+//        }
     }
         .onErrorComplete()
         .subscribeOn(Schedulers.io())
+        .delay(2, TimeUnit.SECONDS)
         .observeOn(AndroidSchedulers.mainThread())
 }
 
