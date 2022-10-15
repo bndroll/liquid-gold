bfb11fc952eb87f4740a2cd5eb876b1af29fbc66
Artem A <akutin_artem@mail.ru>
2022-10-15 21:42:56 +0400

    updated android (#13)

--- mobile/app/src/main/java/com/example/liquidgold/ProfileActivity.kt
+++ mobile/app/src/main/java/com/example/liquidgold/ProfileActivity.kt
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.liquidgold.dto.getReadableRating
 import com.example.liquidgold.services.getInfo
 import com.example.liquidgold.services.getTask
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 
 class ProfileActivity : AppCompatActivity() {
@@ -20,18 +21,25 @@ class ProfileActivity : AppCompatActivity() {
         val fioView = findViewById<TextView>(R.id.profile_name)
         val ratingView = findViewById<TextView>(R.id.rating)
 
-//        val myTaskView = findViewById<Button>(R.id.myTask)
-//        myTaskView.setOnClickListener{
-//
-//        }
-
         getInfo(this).subscribe {
             fioView.text = it.fio
             ratingView.text = getReadableRating(it.rating)
         }
 
+        val ctx = this
         getTask(this).subscribe {
-
+            MaterialAlertDialogBuilder(ctx)
+                .setMessage(resources.getString(R.string.new_ticket))
+                .setNegativeButton(resources.getString(R.string.new_ticket_decline)) { dialog, which ->
+                    // Respond to negative button press
+                }
+                .setPositiveButton(resources.getString(R.string.new_ticket_accept)) { dialog, which ->
+                    val intent = Intent(this, MapActivity::class.java)
+                    intent.putExtra("lat", it.destination.lat)
+                    intent.putExtra("lon", it.destination.lon)
+                    startActivity(intent)
+                }
+                .show()
         }
     }
 }
