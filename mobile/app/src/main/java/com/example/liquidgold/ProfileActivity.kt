751fdada498ab5ecf52b038b8878a5402f87b075
Artem A <akutin_artem@mail.ru>
2022-10-15 16:37:29 +0400

    v1 (#12)

--- mobile/app/src/main/java/com/example/liquidgold/ProfileActivity.kt
+++ mobile/app/src/main/java/com/example/liquidgold/ProfileActivity.kt
@@ -1,14 +1,15 @@
 package com.example.liquidgold
 
+import android.content.Intent
 import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
-import android.widget.ImageView
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.liquidgold.dto.getReadableRating
 import com.example.liquidgold.services.getInfo
-import java.net.URL
+import com.example.liquidgold.services.getTask
 
 
 class ProfileActivity : AppCompatActivity() {
@@ -19,9 +20,18 @@ class ProfileActivity : AppCompatActivity() {
         val fioView = findViewById<TextView>(R.id.profile_name)
         val ratingView = findViewById<TextView>(R.id.rating)
 
+//        val myTaskView = findViewById<Button>(R.id.myTask)
+//        myTaskView.setOnClickListener{
+//
+//        }
+
         getInfo(this).subscribe {
             fioView.text = it.fio
             ratingView.text = getReadableRating(it.rating)
+        }
+
+        getTask(this).subscribe {
+
         }
     }
 }
